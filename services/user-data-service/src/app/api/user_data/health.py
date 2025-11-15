dc5d213a8d360663c14c4f3d65ec2edca11c4fce
Kirill Batrakov <dl9ceb9@gmail.com>
2025-11-15 18:18:18 +0700

    Update health check

--- services/user-data-service/src/app/api/user_data/health.py
+++ services/user-data-service/src/app/api/user_data/health.py
@@ -1,10 +1,10 @@
-from aiokafka import AIOKafkaProducer
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy import text
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.config.config import settings
 from app.database.database import get_async_session
+from app.external_services.kafka_consumer import KafkaConsumerService
 
 router = APIRouter()
 
@@ -29,17 +29,16 @@ async def readiness_probe(
             status_code=503, detail=f'Database connection failed: {e}'
         ) from e
 
-    producer = None
+    consumer = None
     try:
-        producer = AIOKafkaProducer(bootstrap_servers=settings.KAFKA_BOOTSTRAP_SERVERS)
-        await producer.start()
-        await producer.client.fetch_all_metadata()
+        consumer = KafkaConsumerService(config=settings)
+        await consumer.start()
     except Exception as e:
         raise HTTPException(
             status_code=503, detail=f'Kafka connection failed: {e}'
         ) from e
     finally:
-        if producer:
-            await producer.stop()
+        if consumer:
+            await consumer.stop()
 
     return {'status': 'ready'}
