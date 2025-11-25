b73a67d4f3915bb89ad8952be9aaef33ac7068d0
Kirill Batrakov <dl9ceb9@gmail.com>
2025-11-25 16:58:15 +0700

    Спаны теперь в эндпоинты вшиты

--- services/flow-service/src/app/api/products/handler.py
+++ services/flow-service/src/app/api/products/handler.py
@@ -2,6 +2,7 @@ from typing import Any
 
 from fastapi import APIRouter, Depends, HTTPException
 from httpx import ConnectError, HTTPStatusError
+from opentelemetry import trace
 
 from app.dependencies import get_flow_service
 from app.logic.flow_service import FlowService
@@ -9,6 +10,7 @@ from app.logic.flow_service import FlowService
 from .schemas import NumberRequest, ResponseModel
 
 router = APIRouter()
+tracer = trace.get_tracer(__name__)
 
 
 @router.post('/', response_model=ResponseModel)
@@ -19,11 +21,17 @@ async def get_product(
     """
     Возвращает клиенту список доступных ему продуктов.
     """
-    try:
-        return await flow_service.flow_type_selection(request.phone_number)
-    except HTTPStatusError as e:
-        raise HTTPException(status_code=502, detail='Integration Error') from e
-    except ConnectError as e:
-        raise HTTPException(status_code=503, detail='Connect Error') from e
-    except Exception as e:
-        raise HTTPException(status_code=500, detail='Internal Server Error') from e
+    with tracer.start_as_current_span('/api/products') as span:
+        span.set_attribute('http.method', 'PUT')
+        span.set_attribute('user.phone_prefix', request.phone_number[:4])
+        try:
+            return await flow_service.flow_type_selection(request.phone_number)
+        except HTTPStatusError as e:
+            span.record_exception(e)
+            raise HTTPException(status_code=502, detail='Integration Error') from e
+        except ConnectError as e:
+            span.record_exception(e)
+            raise HTTPException(status_code=503, detail='Connect Error') from e
+        except Exception as e:
+            span.record_exception(e)
+            raise HTTPException(status_code=500, detail='Internal Server Error') from e
--- services/scoring-service/src/app/api/scoring/handler.py
+++ services/scoring-service/src/app/api/scoring/handler.py
@@ -1,6 +1,7 @@
 from typing import Any
 
 from fastapi import APIRouter, Depends, HTTPException
+from opentelemetry import trace
 
 from app.core.custom_exceptions import UserNotFoundError
 from app.dependencies import get_scoring_service
@@ -9,6 +10,7 @@ from app.logic.scoring import UserScoring
 from .schemas import ResponseModel, ScoringRequestPioneer, ScoringRequestRepeater
 
 router = APIRouter()
+tracer = trace.get_tracer(__name__)
 
 
 @router.post('/pioneer', response_model=ResponseModel)
@@ -16,14 +18,18 @@ async def get_product_pioneer(
         request: ScoringRequestPioneer,
         scoring_service: UserScoring = Depends(get_scoring_service)
 ) -> dict[str, Any]:
-    try:
-        return await scoring_service.user_scoring_pioneer(
-            user_data=request.user_data,
-            products=request.products
-        )
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail='InternalServerError') from e
+    with tracer.start_as_current_span('/api/scoring/pioneer') as span:
+        span.set_attribute('http.method', 'POST')
+        span.set_attribute('user.phone_prefix', request.phone[:4])
+        try:
+            return await scoring_service.user_scoring_pioneer(
+                user_data=request.user_data,
+                products=request.products
+            )
+        except Exception as e:
+            span.record_exception(e)
+            raise HTTPException(
+                status_code=500, detail='InternalServerError') from e
 
 
 @router.post('/repeater', response_model=ResponseModel)
@@ -32,13 +38,18 @@ async def get_product_repeater(
     scoring_service: UserScoring = Depends(
         get_scoring_service)
 ) -> dict[str, Any]:
-    try:
-        return await scoring_service.user_scoring_repeater(
-            phone=request.phone,
-            products=request.products
-        )
-    except UserNotFoundError as e:
-        raise HTTPException(status_code=404, detail='UserNotFoundError') from e
-    except Exception as e:
-        raise HTTPException(
-            status_code=500, detail='InternalServerError') from e
+    with tracer.start_as_current_span('/api/scoring/repeater') as span:
+        span.set_attribute('http.method', 'POST')
+        span.set_attribute('user.phone_prefix', request.phone[:4])
+        try:
+            return await scoring_service.user_scoring_repeater(
+                phone=request.phone,
+                products=request.products
+            )
+        except UserNotFoundError as e:
+            span.record_exception(e)
+            raise HTTPException(status_code=404, detail='UserNotFoundError') from e
+        except Exception as e:
+            span.record_exception(e)
+            raise HTTPException(
+                status_code=500, detail='InternalServerError') from e
